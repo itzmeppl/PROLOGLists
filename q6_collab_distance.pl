e2262a09bbe3cede7a69ed7c6266c32f507dc5e6
Riddhima <60081089+itzmeppl@users.noreply.github.com>
2023-10-06 14:58:22 -0400

    Add files via upload

---
+++ q6_collab_distance.pl
@@ -0,0 +1,21 @@
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME: 
+%%%%% NAME:
+%%%%% NAME:
+%
+% Add the required rules in the corresponding sections. 
+% If you put the rules in the wrong sections, you will lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the comment lines below
+%
+
+%% To help you test your collabDistance, you may put your knowledge base below. Do not put it under the colladDistance
+%% section. You may also delete your test knowledge based before submission, as we will not be marking it
+
+
+%%%%% SECTION: collabDistance
+%%%%% Predicate definition: collabDist(Author1, Author2, MaxDist, Authors, Articles)
+
