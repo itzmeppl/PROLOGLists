79d62b728025bac39af8fbeebfd80bcbc15f4164
Riddhima <60081089+itzmeppl@users.noreply.github.com>
2023-10-06 14:59:38 -0400

    Update q4_deepest_nesting.pl

--- q4_deepest_nesting.pl
+++ q4_deepest_nesting.pl
@@ -15,3 +15,15 @@
 %%%%% SECTION: deepestNesting
 %%%%% Predicate definition: deepestNesting(List, Depth)
 
+ %func([H|T], Head) :- helper2(H, Head).
+
+%helper2([H|T], Head):- Head = H.
+%header2([], 0).
+
+
+dN([], 0).
+dN([H],0).
+dN([H|T], 1).
+
+dN([H|T], MaxDepth, Acc) :- not is_list(H), dN(T,Acc).
+dN([H|T], D, Acc) :- is_list(H), Depth is Acc+1, max(Depth, D, Y), dN(H, Y).
