3dba7de86c684c902e7d202a3111e25f9385add3
Riddhima <60081089+itzmeppl@users.noreply.github.com>
2023-10-09 20:13:21 -0400

    Update q3_zipper.pl

--- q3_zipper.pl
+++ q3_zipper.pl
@@ -2,8 +2,8 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
+%%%%% NAME: Riddhima Garg
+%%%%% NAME: Piraveen
 %%%%% NAME:
 %
 % Add the required rules in the corresponding sections. 
@@ -14,4 +14,22 @@
 
 %%%%% SECTION: zipper
 %%%%% Predicate definition: zipper(List1, List2, Zipper)
+
+zipper([], L2, [Zipper|L2]).
+zipper(L1, [], [Zipper|L1]).
+
+
+l1zip([], L2, [Zipper|L2]).
+l1zip(L1, [], [Zipper|L1]).
+l1zip([], [], Zipper).
+
+
+l1zip([H1|T1], [H2|T2], Zip) :- append(Zip, [H1,H2], Newzip), l1zip(T1, T2, Newzip). 
+
+
+
+%zipper([H1|T1], [H2|T2], Zipper) :- Newzip is [H1 | H2], zipHelper(T1, T2, Newzip).
+%zipHelper([H1|T1], [H2|T2], Zipper) :- Newzip is [Zipper | [H1, H2]], zip(T1, T2, Newzip).
+
+
 
