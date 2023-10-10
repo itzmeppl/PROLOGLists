fedc33f344f90de0e18daf92dded398e23baec97
Riddhima <60081089+itzmeppl@users.noreply.github.com>
2023-10-10 17:37:49 -0400

    Update q5_partition_months.pl

--- q5_partition_months.pl
+++ q5_partition_months.pl
@@ -15,3 +15,10 @@
 %%%%% SECTION: partitionMonths
 %%%%% Predicate definition: partitionMonths(Months, Prices, Increase, Decrease)
 
+partitionMonths(Months, Prices, Increase, Decrease) :- helper(Months, Prices, Increase, Decrease, [], []).
+
+helper([_,B], [P1,P2], Increase, Decrease, TIncrease, TDecrease) :- P2 >= P1, append(TIncrease, [B], X), Increase = X, Decrease = TDecrease.
+helper([_,B], [P1,P2], Increase, Decrease, TIncrease, TDecrease) :- P2 < P1, append(TDecrease, [B], X), Decrease = X, Increase = TIncrease.
+
+helper([H,H1|T], [HP, HP1|TP], Increase, Decrease, TempIncrease, TempDecrease) :- T \= [], TP \= [], HP1 >= HP, append(TempIncrease, [H1], X), append([H1], T, CurrMonth), append([HP1], TP, CurrPrice), helper(CurrMonth, CurrPrice, Increase, Decrease, X, TempDecrease).
+helper([H,H1|T], [HP, HP1|TP], Increase, Decrease, TempIncrease, TempDecrease) :- T \= [], TP \= [], HP1 < HP, append(TempDecrease, [H1], X), append([H1], T, CurrMonth), append([HP1], TP, CurrPrice), helper(CurrMonth, CurrPrice, Increase, Decrease, TempIncrease, X).
