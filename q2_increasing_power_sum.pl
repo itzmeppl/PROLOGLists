fbb7bc5f1073848cecce78333089d69a7a3f38ba
Riddhima <60081089+itzmeppl@users.noreply.github.com>
2023-10-10 17:51:58 -0400

    Update q2_increasing_power_sum.pl

--- q2_increasing_power_sum.pl
+++ q2_increasing_power_sum.pl
@@ -2,8 +2,8 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
+%%%%% NAME: Piraveen Dayananthan
+%%%%% NAME: Riddhima Garg
 %%%%% NAME:
 %
 % Add the required rules in the corresponding sections. 
